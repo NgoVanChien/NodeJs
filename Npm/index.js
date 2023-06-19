599e79130c8323b3bf86fb47254a22edda90c362
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-19 14:00:38 +0700

    npm & npx

---
+++ Npm/index.js
@@ -0,0 +1,27 @@
+const { sum } = require("lodash");
+
+// const sum = (a, b) => a + b;
+console.log(sum([1, 9]));
+
+// npm outdated
+// npm update
+// npm-check-updates
+
+// ncu
+// run ncu to display packages to upgrade. Similar to npm outdated it will not apply any changes.
+
+// To upgrade dependencies, you just need to run:
+// ncu --upgrade
+
+// or
+// ncu - u;
+
+// npm install
+// -> Apply Changes of packages update (from package.json into package-lock.json )
+
+// npm create-react-app :
+// npx create-react-app :
+// run code in Npx/node_modules/bin/create-react-app
+// If not found package.json or dependencies: create-react-app
+// run create-react-app file in npm.js
+// generate my-app folder in root folder
