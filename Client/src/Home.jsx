ac15210486add6c3384e6684c9b54081b3930e8a
ngovanchien <chienngo03568@gmail.com>
2023-09-26 23:53:15 +0700

    Streaming video

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -40,6 +40,12 @@ export default function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </span>
       </div>
+      <video controls width={500}>
+        <source
+          src="http://localhost:4000/static/video/ef4dedc3e394f60f81ad4b600.mp4"
+          type="video/mp4"
+        />
+      </video>
       <h1>Google OAuth 2.0</h1>
       <p className="read-the-docs">
         {isAuthenticated ? (
