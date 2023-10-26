88e3c316d4ca518ee70adeebc2afa7e1fb7d3b88
ngovanchien <chienngo03568@gmail.com>
2023-10-26 16:49:12 +0700

    Code trang xác thực email bên client React

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -12,7 +12,7 @@ import {
   MediaPoster,
 } from "@vidstack/react";
 
-console.log(import.meta.env);
+// console.log(import.meta.env);
 const getGoogleAuthUrl = () => {
   const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
   const url = `https://accounts.google.com/o/oauth2/v2/auth`;
