2e4d5b9693b8e5b2d23e1fedf57c4da085b030b6
ngovanchien <chienngo03568@gmail.com>
2023-10-28 16:56:56 +0700

    Emit sự kiện

--- Client/src/Chat.jsx
+++ Client/src/Chat.jsx
@@ -6,6 +6,14 @@ export default function Chat() {
     const socket = io(import.meta.env.VITE_API_URL);
     socket.on("connect", () => {
       console.log(socket.id);
+
+      // receive a message from the Server
+      socket.on("hello", (arg) => {
+        console.log(arg);
+      });
+
+      // send a message to the server
+      socket.emit("hi", "This is message was sent from the Client");
     });
     socket.on("disconnect", () => {
       console.log(socket.id); // undefined
